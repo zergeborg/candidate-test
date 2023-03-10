7ef3290ebb73c050290d026e8737cc2c4bcb8d2f
zergeborg <zergeborg@gmail.com>
2023-03-10 09:27:07 +0900

    First commit

---
+++ src/test/java/dev/linkx/assignment/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.linkx.assignment.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
